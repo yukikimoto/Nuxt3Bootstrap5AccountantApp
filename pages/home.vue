c72526eb9de6401a917e9c1f3934e880da67b0ec
Yuki Kimoto <103980824+yukikimoto@users.noreply.github.com>
2023-06-26 05:45:24 +0000

    Add home page

--- pages/home.vue
+++ pages/home.vue
@@ -2,30 +2,30 @@
   <div>
     <nav class="navbar navbar-expand-lg navbar-dark bg-dark">
       <div class="container">
-        <a class="navbar-brand" href="#">財務会計アプリ</a>
+        <NuxtLink class="navbar-brand" to="/">財務会計アプリ</NuxtLink>
         <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
           <span class="navbar-toggler-icon"></span>
         </button>
         <div class="collapse navbar-collapse" id="navbarNav">
           <ul class="navbar-nav">
             <li class="nav-item">
-              <a class="nav-link" href="#">ホーム</a>
+              <NuxtLink class="nav-link" to="/">ホーム</NuxtLink>
             </li>
             <li class="nav-item">
-              <a class="nav-link" href="#">取引履歴</a>
+              <NuxtLink class="nav-link" to="#">取引履歴</NuxtLink>
             </li>
             <li class="nav-item">
-              <a class="nav-link" href="#">設定</a>
+              <NuxtLink class="nav-link" to="#">設定</NuxtLink>
             </li>
           </ul>
         </div>
       </div>
     </nav>
     <div class="container">
-        <h1>財務会計アプリへようこそ！</h1>
-        <p>このアプリは、財務会計の基本的な機能を備えたアプリです。</p>
-        <button class="btn btn-primary">初めての方はこちら</button>
-        <button class="btn btn-primary">登録済みの方はこちら</button>
+        <h1>Homeページ</h1>
+        <!-- ここからHomeを作っていく -->
+        <!-- 財務会計に必要な要素を入れていく -->
+        
 
     </div>
   </div>
